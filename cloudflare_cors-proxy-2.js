fe785ed00fd9c8bad7d0154812a0f76a684cd4ef
Daniel Gruner <dnlgrnr911@gmail.com>
2020-05-11 23:17:37 +0200

    updated cloudflare cors proxy code

---
+++ cloudflare_cors-proxy-2.js
@@ -0,0 +1,79 @@
+// https://developers.cloudflare.com/workers/templates/pages/cors_header_proxy/
+
+// We support the GET, POST, HEAD, and OPTIONS methods from any origin,
+// and accept the Content-Type header on requests. These headers must be
+// present on all responses to all CORS requests. In practice, this means
+// all responses to OPTIONS requests.
+const corsHeaders = {
+  // "Access-Control-Allow-Origin": "http://localhost:3000",
+  "Access-Control-Allow-Origin": "https://mindactuate.github.io",
+  "Access-Control-Allow-Methods": "GET, HEAD, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+};
+
+async function handleRequest(event) {
+  let request = event.request;
+  const url = new URL(request.url)
+  const apiurl = url.searchParams.get("apiurl");
+
+  // Rewrite request to point to API url. This also makes the request mutable
+  // so we can add the correct Origin header to make the API server think
+  // that this request isn't cross-site.
+  request = new Request(apiurl, request);
+  // request.headers.set("Origin", new URL(apiurl).origin);
+
+  let response = await fetch(request);
+
+  // Recreate the response so we can modify the headers
+  response = new Response(response.body, response);
+
+  // Set CORS headers
+  Object.keys(corsHeaders).map(headerName => {
+    response.headers.set(headerName, corsHeaders[headerName]);
+  })
+
+  // Append to/Add Vary header so browser will cache response correctly
+  response.headers.append("Vary", "Origin");
+
+  // Debug
+  response.headers.set("X-Debug-1", JSON.stringify(event));
+  response.headers.set("X-Debug-2", JSON.stringify([...event.request.headers]));
+
+  return response;
+}
+
+function handleOptions(event) {
+  let request = event.request;
+  // Make sure the necesssary headers are present
+  // for this to be a valid pre-flight request
+  return new Response(null, {
+    headers: {
+      ...corsHeaders,
+      "X-Debug-1": JSON.stringify(event),
+      "X-Debug-2": JSON.stringify([...event.request.headers]),
+    },
+  });
+}
+
+addEventListener("fetch", (event) => {
+  const request = event.request;
+  if (request.method === "OPTIONS") {
+    // Handle CORS preflight requests
+    event.respondWith(handleOptions(event));
+  } else if (
+    request.method === "GET" ||
+    request.method === "HEAD" ||
+    request.method === "POST"
+  ) {
+    // Handle requests to the API server
+    event.respondWith(handleRequest(event));
+  } else {
+    event.respondWith(async () => {
+      return new Response(null, {
+        status: 405,
+        statusText: "Method Not Allowed",
+      });
+    });
+  }
+});
--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 let config = {
     // quipHost: "https://platform.quip.com/1/"
-    quipHost: "https://old-rain-a483.mindactuate.workers.dev/?https://platform.quip.com/1/" // 100.000 per day, 1.000 per 10 minutes
+    // quipHost: "https://old-rain-a483.mindactuate.workers.dev/?https://platform.quip.com/1/" // 100.000 per day, 1.000 per 10 minutes
+    quipHost: "https://old-rain-a483.mindactuate.workers.dev/?apiurl=https://platform.quip.com/1/" // 100.000 per day, 1.000 per 10 minutes
 }
 
 export default config;
